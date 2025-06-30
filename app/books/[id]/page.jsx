6bd4b720b484467182f1352917ee41323c3508d1
SatishPrakasham <112928636+SatishPrakasham@users.noreply.github.com>
2025-06-30 15:41:14 +0800

    updated book page

--- app/books/[id]/page.jsx
+++ app/books/[id]/page.jsx
@@ -6,7 +6,8 @@ import { getBookById } from "@/lib/openLibraryApi";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { Button } from "@/components/ui/button"; // You can use a button component from your design system
+import { Button } from "@/components/ui/button";
+import { AddToListButton } from "@/components/AddToListButton";
 
 export default function BookDetailsPage() {
   const [book, setBook] = useState(null);
